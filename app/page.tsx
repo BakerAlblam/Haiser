da26b41deb648e22331d0c5f93742a4ef98a22ab
BakerAlblam <gallardo0@outlook.com>
2023-10-29 16:58:13 +0100

    fml!

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import About from '@/components/About'
 import FooterBasic from '@/components/FooterBasic'
 import Form from '@/components/Form'
-import Navbar from '@/components/Navbar'
 export default function Home() {
   return (
     <div className=''>
-    <Navbar />
+  
         <Form />
         <hr className="h-0.5  mx-auto rounded border-1 bg-white "  />
         <About />
